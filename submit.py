ec4138f101e4a47ea577a604a0fb077e537092b1
xxiann <linxiaoxian18@gmail.com>
2022-11-09 08:05:41 +0800

    .py convert to submission file

--- submit.py
+++ submit.py
@@ -5,17 +5,17 @@ def main():
     # evaluation ids of citeseq-specific cells - (view script.ipynb to obtain)
     evaluation = pd.read_csv('submission/evaluation_ids_cite.csv')
 
-    # test target values
-    target = pd.read_csv('output/base_preds.csv', index_col=0)
+    # test target values - cell_id is stored in first row
+    target = pd.read_csv('output/base_preds.csv')
 
     # converting the format of output
-    target['cell_id'] = target.index # adding cell_id as a column
+    target.rename(columns={'Unnamed: 0':'cell_id'}, inplace=True) # renaming to first column to 'cell_id'
     target = target.melt(id_vars=['cell_id'], value_name='target', var_name='gene_id')
 
     # merging to find corr row id
     target = pd.merge(target, evaluation, how='left', on=['cell_id','gene_id'])
-    target = target['row_id','target']
-    target.to_csv('output/target_submission.csv', index=False)
+    target['row_id','target'].to_csv('output/target_submission.csv', index=False)
 
 if __name__ == "__main__":
     main()
+
