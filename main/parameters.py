04a627e71b5c0fb26c451586fcc3399006b11ba4
xxiann <111058844+xxiann@users.noreply.github.com>
2022-11-26 21:01:33 +0800

    Update parameters.py

--- main/parameters.py
+++ main/parameters.py
@@ -2,13 +2,13 @@
 VERBOSE = 1
 LEARNING_RATE = 0.0005
 BATCH_SIZE = 50
-NUM_EPOCHS = 33
+NUM_EPOCHS = 25
 DROPOUT = 0.05
 VAL_FRAC = 0.0
 device_str = 'cpu'
 
 LOSS = "CorrError"
 
-## do cross validation
-CV = 1
+## do cross validation; set CV to 1
+CV = 0
 FOLD = 5
